0cdb855a4bdd3963fa5a11d3b6700edcf6cba5a5
a.teresa <aseret1221@yahoo.com>
2012-11-13 09:58:59 +0800

    updated frontend/home, updated the style.css, created the feedback,options and cfd pages

---
+++ apps/frontend/modules/static/actions/actions.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * static actions.
+ *
+ * @package    DOUBLECLICK
+ * @subpackage static
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class staticActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+   public function executeFeedback(sfWebRequest $request)
+  {
+    
+  }
+   public function executeOption(sfWebRequest $request)
+  {
+    
+  }
+   public function executeCfd(sfWebRequest $request)
+  {
+    
+  }
+}
---
+++ apps/frontend/modules/static/templates/cfdSuccess.php
@@ -0,0 +1,102 @@
+<!-- content -->
+<div class="contentWrapper">
+  <div class="content">
+    <div class="contentLeft">
+      <a href="" alt="" title=""><img src="/images/content-signup.png" alt="" title=""></a>
+      
+      <div class="contentLeftLogin">
+        <div class="contentLeftLoginHeader">会員ログイン</div>
+        <div class="contentLeftLoginContent">
+          <div><input type="text" placeholder="メールアドレス" /></div>
+          <div><input type="text" placeholder="パスワード" /></div>
+          
+          <input type="checkbox" /><span>次回から自動的にログイン</span>
+          <input type="submit" value="">
+          
+          <a href="" alt="" title="">パスワードを忘れた方</a>
+        </div>
+      </div>
+      
+      <div class="contentLeftColumn">
+        <div class="contentLeftColumnHeader">コラム</div>
+        <div class="contentLeftColumnContent">
+          <ul>
+            <li>
+              <span>2012.10.02</span>
+              <p>暁投資顧問定期コラム Vol. 5</p>
+            </li>
+            <li>
+              <span>2012.10.02</span>
+              <p>暁投資顧問定期コラム Vol. 4</p>
+            </li>
+            <li>
+              <span>2012.10.02</span>
+              <p>暁投資顧問定期コラム Vol. 3</p>
+            </li>
+            <li>
+              <span>2012.10.02</span>
+              <p>暁投資顧問定期コラム Vol. 2</p>
+            </li>
+            <li>
+              <span>2012.10.02</span>
+              <p>暁投資顧問定期コラム Vol. 1</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+      <a href="" alt="" title="" class="contentLeftLoad"><img src="/images/content-load.png" alt="" title=""></a>
+      <a href="" alt="" title="" class="contentLeftReason"><img src="/images/content-reason.png" alt="" title=""></a>
+      
+      <div class="contentLeftMenu">
+        <ul>
+          <li><a href="" alt="" title="" class="contentLeftMenu1"><span>法令順守</span></a></li>
+          <li><a href="" alt="" title="" class="contentLeftMenu2"><span>利用規約</span></a></li>
+          <li><a href="" alt="" title="" class="contentLeftMenu3"><span>契約前事前交付書面</span></a></li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="contentContent">
+    <div class="contentContentOption">
+        <div class="contentContentOptionHeader">
+          CFDとは？
+        </div>
+        <div class="contentContentOptionContent">
+          <div class="contentContentOptionContentDetails">
+            <p>CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。CFDといは？の原稿が入ります。</p> 
+          </div>
+            <ul class="contentContentOptionDetails">
+             <li>
+              チャート①
+              <div class="contentContentOptionContentDetails">
+              <img src="/images/teachers-no-image.png" />
+              </div>
+             </li>
+             <li>
+              利益
+              <div class="contentContentOptionContentDetails">
+                <p>オプションとは？の原稿が入ります。オプションとは？の原稿が入ります。オプションとは？の原稿が入ます。オプションとは？の原稿が入ります。オプションとは？の原稿が入ります。オプションとは？の原稿が入ります。</p>
+              </div>
+             </li>
+             <li>
+              チャート②
+              <div class="contentContentOptionContentDetails">
+              <img src="/images/teachers-no-image.png" />
+              </div>
+             </li>
+             <li>
+              説明
+              <div class="contentContentOptionContentDetails">
+                <p>説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。説明本文が入ります。</p>
+              </div>
+             </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+      </div>
+    </div>
+  </div><!-- end-content -->    
+    <div class="clear"></div>
+</div>
+</div><!-- end-contentWrapper -->
